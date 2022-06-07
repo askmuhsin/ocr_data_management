266777f8f783e34c86cc13f0887ad696164b7f4c
Muhsin <askmuhsin@gmail.com>
2022-06-07 18:12:43 +0530

    update up script to include env variables

--- ocr_services/scripts/up.sh
+++ ocr_services/scripts/up.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 docker build -t ocr_service:latest .
-docker run --name ocr_service_container -p 8123:8123 ocr_service:latest
+docker run \
+    --name ocr_service_container \ 
+    -p 8123:8123 ocr_service:latest \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \ 
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+
+
