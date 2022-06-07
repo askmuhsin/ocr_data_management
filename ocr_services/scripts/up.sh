c860be3fc2278c5eb6e9f1dddb027a77bfa1cd28
Muhsin <askmuhsin@gmail.com>
2022-06-07 18:30:18 +0530

    update up script

--- ocr_services/scripts/up.sh
+++ ocr_services/scripts/up.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-docker build -t ocr_service:latest .
+echo "docker build ... "
+# docker build -t ocr_service:latest . ;
+
+echo "docker run ...";
 docker run \
-    --name ocr_service_container \ 
-    -p 8123:8123 ocr_service:latest \
-    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \ 
-    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-
-
+    -p 8123:8123 \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    --name ocr_service_container ocr_service:latest;
